9e4bedf36f420f7795b8473a1c1b2aaf86e04794
Rafael Lucena <rafael.lucena@arctouch.com>
2020-07-28 19:10:08 -0300

    Create DEVAPPS2019.2 folder and first project

---
+++ DEVAPPS-2019-2-Authentication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DEVAPPS-2019-2-Authentication
+//
+//  Created by Rafael Lucena on 7/27/20.
+//  Copyright © 2020 Rafael Lucena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
